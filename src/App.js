024cdb77df63917aa34bd1dc6b0fcb9168e9b41a
Владислав <pahnetstarushkami@bk.ru>
2024-09-01 23:03:48 +0300

    Создал общую структуру

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://pngimg.com/d/magen_david_PNG16.png" />
       </header>
+      <nav className="nav">
+        <div>
+          <a>PROFILE</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className="content">
+        <div>
+          <img src="https://resetdoc.b-cdn.net/wp-content/uploads/2021/11/ebrei-amazigh_slider.jpg" />
+        </div>
+        <div>Avatar + description</div>
+        <div>
+          My posts
+          <div>New post</div>
+          <div>
+            <div>Post 1</div>
+            <div>Post 2</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
