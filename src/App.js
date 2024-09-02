a0fd447c14bcab23e8d3bf7e9414b3ca49fa1de7
Владислав <pahnetstarushkami@bk.ru>
2024-09-03 00:52:59 +0300

    Поделил на компоненты

--- src/App.js
+++ src/App.js
@@ -1,43 +1,15 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 const App = () => {
   return (
     <div className="app-wrapper">
-      <header className="header">
-        <img src="https://pngimg.com/d/magen_david_PNG16.png" />
-      </header>
-      <nav className="nav">
-        <div>
-          <a>PROFILE</a>
-        </div>
-        <div>
-          <a>Messages</a>
-        </div>
-        <div>
-          <a>News</a>
-        </div>
-        <div>
-          <a>Music</a>
-        </div>
-        <div>
-          <a>Settings</a>
-        </div>
-      </nav>
-      <div className="content">
-        <div>
-          <img src="https://resetdoc.b-cdn.net/wp-content/uploads/2021/11/ebrei-amazigh_slider.jpg" />
-        </div>
-        <div>Avatar + description</div>
-        <div>
-          My posts
-          <div>New post</div>
-          <div>
-            <div>Post 1</div>
-            <div>Post 2</div>
-          </div>
-        </div>
-      </div>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 };
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://resetdoc.b-cdn.net/wp-content/uploads/2021/11/ebrei-amazigh_slider.jpg" />
+      </div>
+      <div>Avatar + description</div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
