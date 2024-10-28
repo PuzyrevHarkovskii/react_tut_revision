aa45db1748ef53bcd09df627da0197c4f418d3a4
Владислав <pahnetstarushkami@bk.ru>
2024-10-28 11:59:01 +0300

    22 lesson

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import style from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={style.dialogs}>
+      <div className={style.dialogItems}>
+        <div className={style.dialog + " " + style.active}>Dimych</div>
+        <div className={style.dialog}>Sasha</div>
+        <div className={style.dialog}>Sveta</div>
+        <div className={style.dialog}>Viktor</div>
+        <div className={style.dialog}>Valera</div>
+      </div>
+      <div className={style.messages}>
+        <div className={style.dialog}>Hi</div>
+        <div className={style.dialog}>How r u?</div>
+        <div className={style.dialog}>Yo</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,49 @@
 import React from "react";
 import style from "./Navbar.module.css";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className={style.nav}>
       <div className={style.item}>
-        <a href="/profile">PROFILE</a>
+        <NavLink
+          to="/profile"
+          className={({ isActive }) => (isActive ? style.active : undefined)}
+        >
+          PROFILE
+        </NavLink>
       </div>
       <div className={style.item}>
-        <a href="/dialogs">Messages</a>
+        <NavLink
+          to="/dialogs"
+          className={({ isActive }) => (isActive ? style.active : undefined)}
+        >
+          Messages
+        </NavLink>
       </div>
       <div className={style.item}>
-        <a href="/news">News</a>
+        <NavLink
+          to="/news"
+          className={({ isActive }) => (isActive ? style.active : undefined)}
+        >
+          News
+        </NavLink>
       </div>
       <div className={style.item}>
-        <a href="/music">Music</a>
+        <NavLink
+          to="/music"
+          className={({ isActive }) => (isActive ? style.active : undefined)}
+        >
+          Music
+        </NavLink>
       </div>
       <div className={style.item}>
-        <a href="/settings">Settings</a>
+        <NavLink
+          to="/settings"
+          className={({ isActive }) => (isActive ? style.active : undefined)}
+        >
+          Settings
+        </NavLink>
       </div>
     </nav>
   );
